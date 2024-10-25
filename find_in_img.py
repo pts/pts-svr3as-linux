552dc4cae0a129e849764bfaf3ce3332b57e747d
Peter Szabo <pts@fazekas.hu>
2024-10-25 04:16:21 +0200

    added import of svr3as-1988-05-27.svr3 and svr3ld-1988-05-27.svr3

--- find_in_img.py
+++ find_in_img.py
@@ -5,11 +5,16 @@ import sys
 
 
 def main(args):
-  bd = open(b'import.tmp/Interactive UNIX386 2.2 rel3.2 (5\xc2\xbc)/disk23.img', 'rb').read()
-  a = open('svr3as-1989-10-03.svr3', 'rb')  # incbin ?, 0xbc00, 0x1b792
+  #bd = open(b'import.tmp/Interactive UNIX386 2.2 rel3.2 (5\xc2\xbc)/disk23.img', 'rb').read()
+  #a = open('svr3as-1989-10-03.svr3', 'rb')  # incbin ?, 0xbc00, 0x1b792
   #a = open('svr3ld-1989-10-03.svr3')  # incbin ?, 0x6e800, 0x19a82
-  bs = 1 << 17  # Block size.
+  #bs = 1 << 17  # Block size.
   # bs = 0x100
+
+  bd = open(b'import.tmp/sds1.img', 'rb').read()
+  #a = open('svr3as-1988-05-27.svr3', 'rb')  # incbin ?, 0x21b82, 0x1b774
+  a = open('svr3ld-1988-05-27.svr3')  # incbin ?, 0x3d349, 0x19a64
+  bs = 1 << 17  # Block size.
 
   i = -1
   prev_ofs = None
--- import.sh
+++ import.sh
@@ -23,6 +23,7 @@ cd "$mydir"
 do_del_tmp=
 for f in "$@"; do  # Import each specified file.
   hash="$(sha256sum <"$f")"
+
   if test "$hash" = "8b9922b071c35bfeb5ad3bfa20a97089668a0b835bf40e35f345a3de12b39135  -"; then  # import_src/Interactive UNIX386 2.2 rel3.2 (5.25).7z
     # Download from https://winworldpc.com/download/405f52c3-bd18-c39a-11c3-a4e284a2c3a5
     # Interactive UNIX386 Release 3.2 Version 2.2 (5.25): `Interactive UNIX386 2.2 rel3.2 (5.25).7z`, 6586165 bytes.
@@ -31,17 +32,18 @@ for f in "$@"; do  # Import each specified file.
     case "$mydir" in /*) mydira="$mydir" ;; *) mydira="$PWD/$mydir" ;; esac
     rm -rf import.tmp
     do_del_tmp=1
-    mkdir  import.tmp
+    mkdir import.tmp
     (cd import.tmp && "$mydira/tools/tiny7zx" x "$f") || exit "$?"
     f="import.tmp/Interactive UNIX386 2.2 rel3.2 (5¼)/disk23.img"
     hash="$(sha256sum <"$f")"
     test "$hash" = "83beba85b33469d17b2622f4748edfd071f73e389d9588af0a57407937ca0f0e  -"
+    # Fall through.
   fi
   if test "$hash" = "83beba85b33469d17b2622f4748edfd071f73e389d9588af0a57407937ca0f0e  -"; then  # Interactive UNIX386 2.2 rel3.2 (5¼)/disk23.img
     do_del_tmp=1
     test -d import.tmp || mkdir import.tmp
     case "$f" in /*) ff="$f" ;; *) f="./$f"; ff="../$f" ;; esac
-    rm -f import.tmp/f.bin
+    rm -f import.tmp/f.bin import.tmp/f.nasm import.tmp/*.svr3
     ln -s "$ff" import.tmp/f.bin
     echo 'incbin "import.tmp/f.bin", 0xbc00, 0x1b792' >import.tmp/f.nasm
     nasm -O0 -f bin -o import.tmp/svr3as-1989-10-03.svr3 import.tmp/f.nasm
@@ -56,10 +58,57 @@ for f in "$@"; do  # Import each specified file.
     cmp import.tmp/svr3ld-1989-10-03.svr3 svr3ld-1989-10-03.svr3 2>/dev/null || cp -ai import.tmp/svr3ld-1989-10-03.svr3 svr3ld-1989-10-03.svr3
     TZ=GMT touch -d '1989-10-03 12:00:00' import.tmp/svr3as-1989-10-03.svr3 import.tmp/svr3ld-1989-10-03.svr3
   fi
+
+  if test "$hash" = "cee926538286ea8db7c3b477f9a906c75768ed4baeae71db789e7cfdf83a88fc  -"; then  # SYSV_386_3.2_SDS_4.1.5.zip
+    # Download http://bitsavers.org/bits/ATT/SYSV_386/SYSV_386_3.2_SDS_4.1.5.zip
+    # Download from http://bitsavers.org/bits/ATT/SYSV_386/
+    case "$f" in /*) ff="$f" ;; *) f="./$f"; ff="../$f" ;; esac
+    rm -f import.tmp/sds1.imd
+    do_del_tmp=1
+    test -d import.tmp || mkdir import.tmp
+    unzip -p "$f" SYSV_386_3.2_SDS_4.1.5/SDS1.IMD >import.tmp/sds1.imd
+    f=import.tmp/sds1.imd
+    hash="$(sha256sum <"$f")"
+    test "$hash" = "69c306d2fe96b2f686aea044e7a1ea6df65e52f4cdc3d59f90f3e82c78f19d8e  -"
+    # Fall through.
+  fi
+  if test "$hash" = "69c306d2fe96b2f686aea044e7a1ea6df65e52f4cdc3d59f90f3e82c78f19d8e  -"; then  # SYSV_386_3.2_SDS_4.1.5/SDS1.IMD
+    case "$f" in /*) ff="$f" ;; *) f="./$f"; ff="../$f" ;; esac
+    rm -f import.tmp/sds1.import.imd import.tmp/sds1.img
+    do_del_tmp=1
+    test -d import.tmp || mkdir import.tmp
+    ln -s "$ff" import.tmp/sds1.import.imd  # disk-analyse requires the .imd extension.
+    disk-analyse import.tmp/sds1.import.imd import.tmp/sds1.img  # Slow.
+    f=import.tmp/sds1.img
+    hash="$(sha256sum <"$f")"
+    test "$hash" = "5577d763edd9b0a80174ac229f76da602ef77abe5007df98b5b372e4c85f1c10  -"
+    # Fall through.
+  fi
+  if test "$hash" = "5577d763edd9b0a80174ac229f76da602ef77abe5007df98b5b372e4c85f1c10  -";  then   # sds1.img
+    do_del_tmp=1
+    test -d import.tmp || mkdir import.tmp
+    case "$f" in /*) ff="$f" ;; *) f="./$f"; ff="../$f" ;; esac
+    rm -f import.tmp/f.bin import.tmp/f.nasm import.tmp/*.svr3
+    ln -s "$ff" import.tmp/f.bin
+    echo 'incbin "import.tmp/f.bin", 0x21b82, 0x1b774' >import.tmp/f.nasm
+    nasm -O0 -f bin -o import.tmp/svr3as-1988-05-27.svr3 import.tmp/f.nasm
+    test "$(sha256sum <import.tmp/svr3as-1988-05-27.svr3)" = "ab7048e14136b142c0264d4f13e9771a05c489661acab562b37929931c0f4c04  -"
+    cmp import.tmp/svr3as-1988-05-27.svr3 svr3as-1988-05-27.svr3 2>/dev/null || cp -ai import.tmp/svr3as-1988-05-27.svr3 svr3as-1988-05-27.svr3
+    #
+    rm -f import.tmp/f.bin
+    ln -s "$ff" import.tmp/f.bin
+    echo 'incbin "import.tmp/f.bin", 0x3d349, 0x19a64' >import.tmp/f.nasm
+    nasm -O0 -f bin -o import.tmp/svr3ld-1988-05-27.svr3 import.tmp/f.nasm
+    test "$(sha256sum <import.tmp/svr3ld-1988-05-27.svr3)" = "9205364de3df93659ede3ac8d2dabe76151c860090af9b5a71f9ff0edef64d16  -"
+    cmp import.tmp/svr3ld-1988-05-27.svr3 svr3ld-1988-05-27.svr3 2>/dev/null || cp -ai import.tmp/svr3ld-1988-05-27.svr3 svr3ld-1988-05-27.svr3
+    TZ=GMT touch -d '1988-05-27 12:00:00' import.tmp/svr3as-1988-05-27.svr3 import.tmp/svr3ld-1988-05-27.svr3
+  fi
   if test "$do_del_tmp"; then rm -rf import.tmp; fi
 done
 
 test ! -f svr3as-1989-10-03.svr3 || test "$(sha256sum <svr3as-1989-10-03.svr3)" = "f6bbc0d5332bc5c12d471ff8fb430b644b21ceaf2037b3572d4f1d01a7a223d0  -"
 test ! -f svr3ld-1989-10-03.svr3 || test "$(sha256sum <svr3ld-1989-10-03.svr3)" = "fd019d3e9b3fd5608c3d3bab28ac45d5a3d9d76751ffdea07bc182227d619e14  -"
+test ! -f svr3as-1988-05-27.svr3 || test "$(sha256sum <svr3as-1988-05-27.svr3)" = "ab7048e14136b142c0264d4f13e9771a05c489661acab562b37929931c0f4c04  -"
+test ! -f svr3ld-1988-05-27.svr3 || test "$(sha256sum <svr3ld-1988-05-27.svr3)" = "9205364de3df93659ede3ac8d2dabe76151c860090af9b5a71f9ff0edef64d16  -"
 
 : "$0" OK.
