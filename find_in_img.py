0901333c9248022f7592c42575a25e03976bed3f
Peter Szabo <pts@fazekas.hu>
2024-10-25 03:43:07 +0200

    added find_in_img.py

---
+++ find_in_img.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+# This program needs Python 2.4--2.7 or 3.x.
+
+import sys
+
+
+def main(args):
+  bd = open(b'import.tmp/Interactive UNIX386 2.2 rel3.2 (5\xc2\xbc)/disk23.img', 'rb').read()
+  a = open('svr3as-1989-10-03.svr3', 'rb')  # incbin ?, 0xbc00, 0x1b792
+  #a = open('svr3ld-1989-10-03.svr3')  # incbin ?, 0x6e800, 0x19a82
+  bs = 1 << 17  # Block size.
+  # bs = 0x100
+
+  i = -1
+  prev_ofs = None
+  while 1:
+    i += 1
+    data = a.read(bs)
+    if not data:
+      break
+    #data2 = data
+    #if len(data) < bs:
+    #  data2 = data + '\0' * (bs - len(data))
+    if prev_ofs is not None and bd[prev_ofs + bs : prev_ofs + bs + len(data)] == data:
+      ofs = prev_ofs + bs
+    else:
+      ofs = bd.find(data)
+      assert ofs >= 0, i
+    assert bd[ofs : ofs + len(data)] == data
+    print('incbin ?, 0x%x, 0x%x' % (ofs, len(data)))
+    prev_ofs = ofs
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
